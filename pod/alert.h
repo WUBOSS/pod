f3b6a7ef036d0b64d2deb4925eb0d998299f040f
zhanshu1 <1054258896@qq.com>
2017-10-11 09:59:56 +0800

    text

---
+++ pod/alert.h
@@ -0,0 +1,13 @@
+//
+//  alert.h
+//  podText
+//
+//  Created by zhanshu1 on 2017/10/11.
+//  Copyright © 2017年 WU. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface alert : NSObject
+-(void)alert;
+@end
---
+++ pod/alert.m
@@ -0,0 +1,16 @@
+//
+//  alert.m
+//  podText
+//
+//  Created by zhanshu1 on 2017/10/11.
+//  Copyright © 2017年 WU. All rights reserved.
+//
+
+#import "alert.h"
+
+@implementation alert
+-(void)alert
+{
+    NSLog(@"ios pod");
+}
+@end
